8322f206d1b9eeafd09263471d26d843484630f7
Ignacio Vaggione <ivaggione@gmail.com>
2020-08-06 17:02:47 +0000

    For old board editor as well

--- board_editor/js/index.js
+++ board_editor/js/index.js
@@ -5,6 +5,7 @@ import 'react-rangeslider/lib/index.css';
 import 'react-select/dist/react-select.css';
 import '../css/board-edit.scss';
 import BoardsEditorApp from './BoardsEditorApp';
+import 'bootstrap/dist/js/bootstrap.js'
 
 
 ReactDOM.render(<BoardsEditorApp />,

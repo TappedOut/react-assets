e8696458c6080aa6c14c88ba6425afa382778003
Ignacio Vaggione <ivaggione@gmail.com>
2020-09-18 09:47:49 +0000

    Dont import bootstrap again

--- board_editor/js/index.js
+++ board_editor/js/index.js
@@ -5,7 +5,6 @@ import 'react-rangeslider/lib/index.css';
 import 'react-select/dist/react-select.css';
 import '../css/board-edit.scss';
 import BoardsEditorApp from './BoardsEditorApp';
-import 'bootstrap/dist/js/bootstrap.js'
 
 
 ReactDOM.render(<BoardsEditorApp />,

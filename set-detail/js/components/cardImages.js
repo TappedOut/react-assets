7009ecc0f5f836c8d7f4e1ae2be400d96994b883
Ignacio Vaggione <ivaggione@gmail.com>
2024-10-16 00:17:06 +0200

    Feedback reworked

--- set-detail/js/components/cardImages.js
+++ set-detail/js/components/cardImages.js
@@ -5,7 +5,10 @@ import CardImage from './cardImage.js'
 export default class CardImages extends React.Component {
   render() {
     const images = this.props.specs.map(spec =>
-      <CardImage default_tla={this.props.default_tla} spec={spec} width={this.props.width} backsides={this.props.backsides} rank_label={this.props.rank_label} choices={this.props.choices}/>
+      <CardImage default_tla={this.props.default_tla} spec={spec} width={this.props.width} 
+                 feedbackCB={this.props.feedbackCB} feedback_enabled={this.props.feedback_enabled}
+                 backsides={this.props.backsides} rank_label={this.props.rank_label} choices={this.props.choices}
+      />
     )
     return (
       <div style={{"display": "flex", "flex-wrap": "wrap"}}>
--- set-detail/js/components/cardTable.js
+++ set-detail/js/components/cardTable.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {Table} from 'react-bootstrap';
+import {Table, Button, Glyphicon} from 'react-bootstrap';
 import CardSpan from './cardSpan.js'
 
 
 export default class CardTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gaveFeedback: {}
+    }
+  }
+
   render() {
     const choices = this.props.choices;
     const rows = this.props.specs.map(spec => {
@@ -11,6 +18,32 @@ export default class CardTable extends React.Component {
       price_url = price_url ? `${price_url}${choices.vendor_param['CK']}` : ''
       const rank = spec.rank_display !== '--' ? `#${spec.rank_display}` : spec.rank_display;
       return (<tr key={spec.pk}>
+        {this.props.feedbackCB && <td>
+          <Button 
+            bsSize="small" 
+            bsStyle="success" 
+            block
+            onClick={() => {
+              this.props.feedbackCB(spec.pk, 'correct');
+              this.setState({gaveFeedback: {...this.state.gaveFeedback, [spec.pk]: true}})
+            }}
+            disabled={!this.props.feedback_enabled || this.state.gaveFeedback[spec.pk]} 
+          >
+            <Glyphicon glyph="thumbs-up" />
+          </Button>
+          <Button 
+            bsSize="small" 
+            bsStyle="danger" 
+            block
+            onClick={() => {
+              this.props.feedbackCB(spec.pk, 'incorrect');
+              this.setState({gaveFeedback: {...this.state.gaveFeedback, [spec.pk]: true}})
+            }}
+            disabled={!this.props.feedback_enabled || this.state.gaveFeedback[spec.pk]} 
+          >
+            <Glyphicon glyph="thumbs-down" />
+          </Button>
+        </td>}
         <td>
           <CardSpan spec={spec} backside={this.props.backsides[spec['flip']]} tla={this.props.default_tla} />
         </td>
@@ -25,6 +58,7 @@ export default class CardTable extends React.Component {
       <Table striped bordered hover responsive>
         <thead>
           <tr>
+            {this.props.feedbackCB && <th style={{width: '5%'}}></th>}
             <th>Name</th>
             <th>Type</th>
             <th>Mana Cost</th>
